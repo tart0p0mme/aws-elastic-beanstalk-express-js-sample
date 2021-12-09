7f4df5abe71458e64ada8c800afe501869ee531f
robinem <maxime.robine@edu.esiee.fr>
2021-12-09 19:44:55 +0100

    Test de la pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bien le bonjour!'));
+app.get('/', (req, res) => res.send('Test complet de la pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
